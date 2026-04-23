8474cdca39ef3e749395a4e92cdb39a042f7ae2d
salvolannister <darksawyer@hotmail.it>
2026-04-23 17:21:03 +0200

    add copilot suggestions

--- Assets/Scripts/ArcweaveAudioManager.cs
+++ Assets/Scripts/ArcweaveAudioManager.cs
@@ -5,7 +5,7 @@ public class ArcweaveAudioManager : MonoBehaviour
 {
     public static ArcweaveAudioManager Instance { get; private set; }
 
-    public Action<AudioClip> OnAudioClipStop;
+    public event Action<AudioClip> OnAudioClipStop;
 
     private void Awake()
     {
@@ -21,6 +21,15 @@ public class ArcweaveAudioManager : MonoBehaviour
     public void SignalAudioClipStop(AudioClip clip)
     {
         OnAudioClipStop?.Invoke(clip);
+    }
+    public void SubscribeToAudioClipStop(Action<AudioClip> callback)
+    {
+        OnAudioClipStop += callback;
+    }
+
+    public void UnsubscribeFromAudioClipStop(Action<AudioClip> callback)
+    {
+        OnAudioClipStop -= callback;
     }
 
 }
